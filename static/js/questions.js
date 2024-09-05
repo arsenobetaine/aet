5d1d911d0149ab58abddcd1b7067f969a67f5555
sahandirsolei <contact@sahandsoleimani.ir>
2024-09-05 13:18:58 +0330

    AET

---
+++ static/js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "His eyes were ...... bad that he couldn't read the number plate of the car in front.",
+        answer: "C. so",
+        options: [
+            "A. such",
+            "B. too",
+            "C. so",
+            "D. very"
+        ]
+    },
+    {
+        numb: 2,
+        question: "The company needs to decide ...... and for all what its position is on this point.",
+        answer: "B. once",
+        options: [
+            "A. here",
+            "B. once",
+            "C. first",
+            "D. finally"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Don't put your cup on the ...... of the table – someone will knock it off.",
+        answer: "B. edge",
+        options: [
+            "A. outside",
+            "B. edge",
+            "C. boundary",
+            "D. border"
+        ]
+    },
+    {
+        numb: 4,
+        question: "I'm sorry - I didn't ...... to disturb you.",
+        answer: "C. mean",
+        options: [
+            "A. hope",
+            "B. think",
+            "C. mean",
+            "D. suppose"
+        ]
+    },
+    {
+        numb: 5,
+        question: "The singer ended the concert ...... her most popular song.",
+        answer: "B. with",
+        options: [
+            "A. by",
+            "B. with",
+            "C. in",
+            "D. as"
+        ]
+    }
+];
